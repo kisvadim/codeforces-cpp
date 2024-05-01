0fab08094f905e95005cd67bdf5590cb9de44141
Vadim Kiselev <mrKiselev@gmail.com>
2024-05-01 06:40:07 +0300

    141A

--- 141A/141A.cpp
+++ 141A/141A.cpp
@@ -1,34 +1,28 @@
 #include <iostream>
-#include <iterator>
 #include <string>
 
-int main()
-{
-    std::string guest, host, letters;
+int main() {
+  std::string guest, host, letters;
+  std::cin >> guest >> host >> letters;
 
-    std::cin >> guest;
-    std::cin >> host;
-    std::cin >> letters;
+  while (letters.size() != 0) {
+    char c = letters[0];
+    std::size_t found_guest = guest.find(c);
+    std::size_t found_host = host.find(c);
+    if (found_guest != std::string::npos)
+      guest.erase(found_guest, 1);
+    else if (found_host != std::string::npos)
+      host.erase(found_host, 1);
+    else
+      break;
 
-    while (letters.length() != 0)
-    {
-        std::size_t found_guest = guest.find(letters[0]);
-        std::size_t found_host = host.find(letters[0]);
+    letters.erase(0, 1);
+  }
 
-        if(found_guest != std::string::npos)
-            guest.erase(found_guest, 1);
-        else if(found_host != std::string::npos)
-            host.erase(found_host, 1);            
-        else
-            break;
+  if (letters.empty() && guest.empty() && host.empty())
+    std::cout << "YES" << std::endl;
+  else
+    std::cout << "NO" << std::endl;
 
-        letters.erase(0, 1);
-    }
-
-    if(letters.empty() && guest.empty() && host.empty())
-        std::cout << "YES" << std::endl;
-    else 
-        std::cout << "NO" << std::endl;
-
-    return 0;
+  return 0;
 }
